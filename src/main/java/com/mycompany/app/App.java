b01bb6f790d511faae3c7585f420839ee970efca
yeexuen23 <rachelle.yx@gmail.com>
2023-02-27 07:56:15 +0000

    Make a change to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("hi there");
+        System.out.println("hungryyy");
     }
 
     public String getMessage() {
