526d37ff18d84b9cf285955d07ceb364a91a07d6
yeexuen23 <rachelle.yx@gmail.com>
2023-02-27 08:35:11 +0000

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("hungryyy");
+        System.out.println("okay hahah");
     }
 
     public String getMessage() {
