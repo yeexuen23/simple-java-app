a35848dd027ca69c856f78fd9f8f5e9b713babca
yeexuen23 <rachelle.yx@gmail.com>
2023-02-28 05:44:19 +0000

    demo webhook

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,12 +5,12 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "I lol Jenkins";
+    private static final String MESSAGE = "I love Jenkins";
 
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("okay hahah");
+        System.out.println("tireddd");
     }
 
     public String getMessage() {
